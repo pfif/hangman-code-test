db22e65052b34bceaeaa073a4ee633598a3af332
Florent Pastor <florent.pastor@ostmodern.co.uk>
2019-04-01 10:24:31 +0200

    Deleted a useless file

--- hangman/tests/uistate_tests.py
+++
@@ -1,7 +0,0 @@
-from hangman.src.uistate import initial_state
-
-
-# Initial state
-def test_initial_state_current_screen_is_menu():
-    assert "current_screen" in initial_state()
-    assert "menu" == initial_state()["current_screen"]
